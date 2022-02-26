4d063beb83fa5f40a3c4cf4a811b5f98fd2ba9a1
S. Espinet <s.lou.espinet@gmail.com>
2022-02-25 16:18:01 -0800

    get initial passing PastryConstructor test

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -84,17 +84,14 @@ namespace Bakery.Tests
     }
   }
 
-
-
-//   [TestClass]
-//   public class PastryTests
-//   {
-//     [TestMethod]
-//     public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
-//     {
-//       //Example: LeapYear testLeapYear = new LeapYear();
-//       Assert.AreEqual(ExpectedResult, CodeToTest);
-//       //Example CodeToTest: testLeapYear.IsLeapYear(2020)
-//     }
-//   }
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryConstructor_CreateInstanceOfPastry_Pastry()
+    {
+      Pastry pastryOrder = new Pastry();
+      Assert.AreEqual(typeof(Pastry), pastryOrder.GetType());
+    }
+  }
 }
--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -2,21 +2,10 @@ namespace Bakery
 {
   public class Pastry
   {
-    //Example:
-//     public bool IsLeapYear(int year)
-//     {
-//       if (year % 400 == 0)
-//       {
-//         return true;
-//       }
-//       else if (year % 100 == 0)
-//       {
-//         return false;
-//       }
-//       else
-//       {
-//         return year % 4 == 0;
-//       }
-//     }
+
+    public Pastry()
+    {
+
+    }
   }
 }
