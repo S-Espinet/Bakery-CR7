5ccfebde0cdaa1d492175631090fe48fc5cdb6dd
S. Espinet <s.lou.espinet@gmail.com>
2022-02-26 19:05:58 -0800

    add ui logic for Bakery project

---
+++ Bakery/Program.cs
@@ -0,0 +1,43 @@
+using System;
+using Bakery.Models;
+
+namespace Bakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery!\n \n###########################\n \nPierre's Prices:\n \n  Bread: $5/Loaf.....Buy 2, get one free!\n \n  Pastries:\n   Single Pastry.....$2\n   Box of 3.....$5");
+      Console.WriteLine("Please tell us how many loaves of bread and how many pastries you would like to order.");
+      Console.WriteLine(" Loaves of Bread: ");
+      string loafNumber = Console.ReadLine();
+      int userLoafNumber;
+      try
+      {
+        userLoafNumber = int.Parse(loafNumber);
+      }
+      catch
+      {
+        Console.WriteLine("Please enter a numeral (e.g. 4). Exiting program...");
+        return;
+      }
+      Console.WriteLine(" Pastries: ");
+      string pastriesNumber = Console.ReadLine();
+      int userPastriesNumber;
+      try
+      {
+        userPastriesNumber = int.Parse(pastriesNumber);
+      }
+      catch
+      {
+        Console.WriteLine("Please enter a numeral (e.g. 4). Exiting program...");
+        return;
+      }
+      Bread breadOrder = new Bread(userLoafNumber);
+      Pastry pastriesOrder = new Pastry(userPastriesNumber);
+      int totalCost = breadOrder.CalculatePrice() + pastriesOrder.CalculatePastryPrice(); 
+      Console.WriteLine("The total cost of your order is: " + totalCost);
+      return;
+    }
+  }
+}
